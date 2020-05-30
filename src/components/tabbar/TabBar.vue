305101af74bb7c8a2506a49f0d7e908e060887ea
zhangyunpeng0506 <zhangyunpeng0506@163.com>
2020-05-30 10:26:33 +0800

    文件夹重命名

---
+++ src/components/tabbar/TabBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="tab-bar">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'TabBar',
+}
+</script>
+
+<style lang="less" scoped>
+  .tab-bar{
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    height: 49px;
+    background-color:#f6f6f6;
+    box-shadow: 0px 1px 1px #f6f6f6;
+  }
+</style>
---
+++ src/components/tabbar/TabBarItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="tab-bar-item" :style="active">
+    <div @click="itemClick">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name:'TabBarItem',
+  props:{
+    path:{
+      type:String,
+      default:''
+    },
+    activeColor:{
+      type:String,
+      default:'blue'
+    }
+  },
+  computed:{
+    active(){
+      return this.$route.path.includes(this.path)?{color:this.activeColor,'border-bottom':`solid 1px ${this.activeColor}`}:{}
+    }
+  },
+  methods:{
+    itemClick(){
+      this.$router.replace(this.path)
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+  .tab-bar-item{
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>
